2a66aea11a79ef1ccb68fa9b955fb9fafe5da5c2
Vladino <usacev@gmail.com>
2026-08-12 02:26:35 +0200

    Update __init__.py

--- custom_components/abb_egon/__init__.py
+++ custom_components/abb_egon/__init__.py
@@ -5,6 +5,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_PORT, CONF_USERNAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import ABBEgonClient
@@ -42,12 +43,42 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    _async_cleanup_stale_entities(hass, entry, coordinator)
+
     _LOGGER.debug(
         "ABB Egon async_setup_entry done entry_id=%s platforms=%s",
         entry.entry_id,
         PLATFORMS,
     )
     return True
+
+
+def _async_cleanup_stale_entities(hass: HomeAssistant, entry: ConfigEntry, coordinator) -> None:
+    """Remove entities that are no longer part of the selected elements."""
+    if coordinator.selected_element_ids is None:
+        _LOGGER.debug("ABB Egon cleanup skipped, no element filter active")
+        return
+
+    registry = er.async_get(hass)
+    active_ids = coordinator.selected_element_ids
+
+    entries_to_remove = []
+    for entity_entry in list(registry.entities.values()):
+        if entity_entry.config_entry_id != entry.entry_id:
+            continue
+
+        unique_id = entity_entry.unique_id
+        element_id = unique_id.rsplit("_", 1)[-1] if "_" in unique_id else unique_id
+
+        if element_id not in active_ids:
+            entries_to_remove.append(entity_entry.entity_id)
+
+    for entity_id in entries_to_remove:
+        _LOGGER.debug("ABB Egon removing stale entity=%s", entity_id)
+        registry.async_remove(entity_id)
+
+    if entries_to_remove:
+        _LOGGER.debug("ABB Egon cleanup removed=%s entities", len(entries_to_remove))
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
